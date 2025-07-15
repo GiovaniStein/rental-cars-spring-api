788e062ba05b23b402dd07ebac02f260f0ca6390
GiovaniStein <giovanistein10@gmail.com>
2025-07-15 16:31:11 -0300

    fix: ajustes CORS e adição handler

--- src/main/java/com/challenge/rental_cars_spring_api/access/AluguelRestController.java
+++ src/main/java/com/challenge/rental_cars_spring_api/access/AluguelRestController.java
@@ -19,7 +19,6 @@ import java.io.IOException;
 @RestController
 @RequestMapping("/aluguel")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:4200")
 public class AluguelRestController {
 
     private final UploadAluguelService uploadAluguelService;
--- src/main/java/com/challenge/rental_cars_spring_api/access/CarrosRestController.java
+++ src/main/java/com/challenge/rental_cars_spring_api/access/CarrosRestController.java
@@ -20,7 +20,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/carros")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:4200")
 public class CarrosRestController {
 
     private final ListarCarrosQuery listarCarrosQuery;
